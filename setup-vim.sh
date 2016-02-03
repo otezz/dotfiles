2af1349df173ba4655dbd68d7ecb82b773506b9b
Seto Kuslaksono <seto@pegipegi.com>
2016-02-03 12:31:23 +0700

    new install script

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
-# Setup vim
+main() {
+    echo "Setting up vim.."
+    cd ~
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    curl -sO https://raw.githubusercontent.com/otezz/dotfiles/master/.vimrc
+    mkdir -p ~/.vim/swap
+    mkdir -p ~/.vim/backup
+    vim +PluginInstall +qall
+    echo "Done!"
+}
 
-echo "Setting up vim.."
-cd ~
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-curl -sO https://raw.githubusercontent.com/otezz/dotfiles/master/.vimrc
-mkdir -p ~/.vim/swap
-mkdir -p ~/.vim/backup
-vim +PluginInstall +qall
-echo "Done!"
+main
