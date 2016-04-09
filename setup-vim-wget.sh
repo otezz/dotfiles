01f8d45ef2cb1fd2a194fe22fcb048d5fa6f3343
Seto Kuslaksono <kuslaksono@gmail.com>
2016-04-09 17:28:09 +0700

    Add install git

--- setup-vim-wget.sh
+++ setup-vim-wget.sh
@@ -1,7 +1,7 @@
 main() {
     echo "Setting up vim.."
     cd ~
-    sudo apt-get install -y vim
+    sudo apt-get install -y vim git
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     wget https://raw.githubusercontent.com/otezz/dotfiles/master/.vimrc
     mkdir -p ~/.vim/swap
